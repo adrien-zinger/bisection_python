ac10b9a593fb87681c1da0834e5811bfa3bf3503
adrien <azinger@eos-imaging.com>
2021-05-11 09:04:54 +0200

    add a better test

--- test_all.py
+++ test_all.py
@@ -5,12 +5,11 @@ from binary_search import binary_search
 
 def test_bisection():
     def f(x):
-        if x < 5: return -1
-        elif x >= 6: return 1
+        if x < 2: return -1
+        elif x >= 3: return 1
         else: return 0
-    r=bisection(f, 0, 10, 1)
-    print(r)
-    assert r==5
+    r=bisection(f, 0, 10, 3)
+    assert 3
 
 def test_bisec():
     def f(x):
